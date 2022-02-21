a973561c427b19b94c60fc67d513452f7ae66d7a
leidy tapias <leidytapiasgiraldo@gmail.com>
2022-02-20 21:01:27 -0500

    se realiza prueba

--- src/data.js
+++ src/data.js
@@ -1,8 +1,8 @@
 // estas funciones son de ejemplo
 
-export const filterPersonaje = data.films.filter((peliculas) => {
-let filterPersonaje = document.createElement(personaje);
-  return 'personaje'
+export const  anotherExample = () => {
+
+  return 'example'
 }
 
 export const anotherExample = () => {
